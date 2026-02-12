20f0123de1e74259c39ebdbe30e38aa34a56670c
Aniket <aniketsingh2151@gmail.com>
2026-02-12 20:52:55 +0530

    add stopwords file in data path, add tokenizers, to_lower() filter, token matcher and stopword filter in keyword search module

--- cli/lib/keyword_search.py
+++ cli/lib/keyword_search.py
@@ -1,11 +1,42 @@
 from .search_utils import load_movies
+from pathlib import Path
+
+PROJECT_ROOT = Path(__file__).resolve().parents[2]
+DATA_PATH = PROJECT_ROOT/'data'/'stopwords.txt'
+
+import string
 
 def search_command(query: str, top_results: int = None):
     movies = load_movies()
     result = []
+    query = cleanse(query)
     for movie in movies:
-        if query.lower() in movie["title"].lower():
+        if token_match( tokenize(query), tokenize(movie["title"])):
             result.append(movie)
         if len(result) == top_results:
             break
     return result
+
+def cleanse (input: str) -> str:
+    input = input.lower()
+    result = input.translate(str.maketrans("", "", string.punctuation))
+    return input
+
+def tokenize (input : str) -> str:
+    result = cleanse(input)
+    tokens = result.split()
+    tokens = [token for token in tokens if token]
+    return tokens
+
+def token_match (input_tokens: list[str], output_tokens: list[str] ) -> list[str]:
+    output_set = set(output_tokens)
+    for token in filter(input_tokens):
+        if token in output_set:
+            return True
+    return False
+
+def filter (input_tokens: list[str]) -> list[str]:
+    with open(DATA_PATH, "r") as f:
+        stopwords = f.read().splitlines()
+
+    return [token for token in input_tokens if token not in stopwords]
