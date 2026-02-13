8256d959c8b01af6a543861e89878e65544a824c
Aniket <aniketsingh2151@gmail.com>
2026-02-13 14:52:30 +0530

    reorganize functions and arrange a singular preprocessing pipeline, also otken matching now matches the first token to be a equivalent to or a subset of the second token

--- cli/lib/keyword_search.py
+++ cli/lib/keyword_search.py
@@ -1,47 +1,48 @@
 from .search_utils import load_movies
-from pathlib import Path
+from .search_utils import read_stopwords
 from nltk.stem import PorterStemmer
-
-PROJECT_ROOT = Path(__file__).resolve().parents[2]
-DATA_PATH = PROJECT_ROOT/'data'/'stopwords.txt'
 
 import string
 
 def search_command(query: str, top_results: int = None):
     movies = load_movies()
     result = []
-    query = cleanse(query)
     for movie in movies:
-        if token_match( tokenize(query), tokenize(movie["title"])):
+        if token_match( preprocess(query), preprocess(movie["title"])):
             result.append(movie)
         if len(result) == top_results:
             break
     return result
 
+def preprocess(input: str) -> str:
+    input = cleanse(input)
+    input = tokenize(input)
+    input = stem(input)
+    input = filter(input)
+    return input
+
 def cleanse (input: str) -> str:
     input = input.lower()
     result = input.translate(str.maketrans("", "", string.punctuation))
-    return input
+    return result
 
-def tokenize (input : str) -> str:
+def tokenize (input : str) -> list[str]:
     result = cleanse(input)
     tokens = result.split()
     tokens = [token for token in tokens if token]
     return tokens
 
-def token_match (input_tokens: list[str], output_tokens: list[str] ) -> list[str]:
-    output_set = set(filter(stem(output_tokens)))
-    for token in filter(stem(input_tokens)):
-        if token in output_set:
-            return True
-    return False
-
-def filter (input_tokens: list[str]) -> list[str]:
-    with open(DATA_PATH, "r") as f:
-        stopwords = f.read().splitlines()
-
-    return [token for token in input_tokens if token not in stopwords]
-
 def stem (input_tokens: list[str]) -> list[str]:
     stemmer = PorterStemmer()
     return [stemmer.stem(token) for token in input_tokens]
+
+def filter(input_token: list[str]) -> list[str]:
+    return [token for token in input_token if token not in read_stopwords()]
+
+def token_match (input_tokens: list[str], output_tokens: list[str] ) -> list[str]:
+    # output_set = set(output_tokens)
+    for token in input_tokens:
+        for output_token in  output_tokens:
+            if token in output_token:
+                return True
+    return False
--- cli/lib/search_utils.py
+++ cli/lib/search_utils.py
@@ -2,9 +2,16 @@ import json
 from pathlib import Path
 
 PROJECT_ROOT = Path(__file__).resolve().parents[2]
-DATA_PATH = PROJECT_ROOT/'data'/'movies.json'
+MOVIES_PATH = PROJECT_ROOT/'data'/'movies.json'
+STOPWORDS_PATH = PROJECT_ROOT/'data'/'stopwords.txt'
 
 def load_movies() -> list[dict]:
-    with open(DATA_PATH, "r") as f:
+    with open(MOVIES_PATH, "r") as f:
         data = json.load(f)
     return data['movies']
+
+def read_stopwords () -> list[str]:
+    with open(STOPWORDS_PATH, "r") as f:
+        stopwords = f.read().splitlines()
+
+    return stopwords
